1041a1f940031fda28c0ed7ffc938febb6f10482
Olav Fosse <fosseolav@gmail.com>
2020-10-17 20:09:06 +0200

    Extract ping endpoint to routers/ping.ts

--- patientor/src/index.ts
+++ patientor/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import pingRouter from './routes/ping';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/ping', (_req, res) => {
-  res.send('pong');
-});
+app.use('/api/ping', pingRouter);
 
 const port = 3001;
 app.listen(port, () => console.log(`Listening on ${port}`));
