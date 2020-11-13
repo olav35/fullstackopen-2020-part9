c4b0012299c90393a146674edce820f6351a1a30
Olav Fosse <mail@olavfosse.no>
2020-11-13 09:04:27 +0100

    do something

--- patientor-front/src/PatientPage/index.tsx
+++ patientor-front/src/PatientPage/index.tsx
@@ -7,6 +7,45 @@ import { apiBaseUrl } from "../constants";
 import { updatePatient } from '../state/reducer';
 import { setDiagnosises } from '../state/reducer';
 
+
+const EntryDetails: any = (props: {entry: any}) => {
+  /* include the Entry's details with a new component that shows rest of the information of the patients entries distinguishing different types from each other. */
+  switch(props.entry.type) {
+    case 'Hospital':
+      return (
+	<div>
+	  type: hospital<br/>
+	  {props.entry.date} {props.entry.criteria}
+	</div>
+      )
+    case 'OccupationalHealthcare':
+      return (
+	<div>
+	  type: occupational healthcare<br/>
+	  employer: {props.entry.employerName}<br/>
+	{
+	  props.entry.sickLeave ? (
+	    <div>
+	      {props.entry.sickLeave.startDate} -> {props.entry.sickLeave.endDate}
+	    </div>
+	  ): null
+	}
+	</div>
+      )
+    case 'HealthCheck':
+      return (
+	<div>
+	  type: health check<br/>
+	  health check rating: {props.entry.healthCheckRating}
+	</div>
+      )
+    default:
+      console.log('pre')
+      console.log(props.entry.type)
+      return null
+  }
+}
+
 const PatientPage: React.FC = () => {
   const { id } = useParams<{ id: string}>();
   const [state, dispatch] = useStateValue();
@@ -64,31 +103,32 @@ const PatientPage: React.FC = () => {
 	/* date, description and diagnose  */
         state.patients[id].entries ? (() => {
           const entries = state.patients[id].entries as Entry[];
-	  console.log(entries)
-	  return entries.map(entry => (
-	    <div>
-	      <span>{entry.date} {entry.description}</span>
-	      {
-		entry.diagnosisCodes ? (
-		  <ul>
-		    {
-		      entry.diagnosisCodes.map(code => (
-			<li>
-			  {
-			    code
-			  }
-			  {
-			    (() => {
-			      const d = state.diagnosises.find(d => d.code === code)
-			      return d ? ' '+d.name : null
-			    })()
-			  }
-			</li>))
-		    }
-		  </ul>
-		) : null
-	      }
-	    </div>
+	  return entries.map(entry =>
+	    (
+	      <div key={entry.id}>
+		<span>{entry.date} {entry.description}</span>
+		{
+		  entry.diagnosisCodes ? (
+		    <ul>
+		      {
+			entry.diagnosisCodes.map(code => (
+			  <li>
+			    {
+			      code
+			    }
+			    {
+			      (() => {
+				const d = state.diagnosises.find(d => d.code === code)
+				return d ? ' '+d.name : null
+			      })()
+			    }
+			  </li>))
+		      }
+		    </ul>
+		  ) : null
+		}
+		<EntryDetails entry={entry}/>
+	      </div>
 	  ))
         })() : (() => {
           return <p>loading entries</p>;
